4e5e29340c9c1c124e0c67f45da181f914f68466
sebariza <116417136+sebariza@users.noreply.github.com>
2023-09-23 16:40:32 -0300

    Add files via upload

---
+++ contacto/enviar.php
@@ -0,0 +1,27 @@
+<?php
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $fullname . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Telefono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'seba-ariel-ariza1982@hotmail.com';
+$asunto = 'Asunto del mensaje';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
+
+
+
